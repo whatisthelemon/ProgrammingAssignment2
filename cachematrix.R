d2dadc5ac999106721cb2988521fb6d5c7d1e251
whatisthelemon <whatisthelemon@gmail.com>
2015-03-20 17:47:29 -0400

    Began working on assignment. Created functions to test correctness.
    Created function matrixMultiply to multiply two matrices together.
    
    Created function checkInverse to check of one matrix is the inverse of
    another.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,9 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    if(nrow(x) != ncol(x)) stop(error="it is only possible to find the inverse of a square matrix")
+    
+    
 }
 
 
@@ -12,4 +14,43 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    #determine if x is square
+
+}
+
+matrixMultiply <- function(x = matrix(), y=matrix()){
+    if (ncol(x) != nrow(y)) stop(error="Cannot multiply these two matrices. For matrix multiplication,  the number of columns in the first matrix must equal the number of rows in the second")
+    
+    res <- matrix(nrow=nrow(x), ncol=ncol(y))
+    for(j in 1:ncol(res)){
+        for (i in 1:nrow(res)){
+            res[i,j] = sum(x[i,] * y[,j])
+        }
+    }
+    return(res)
+}
+
+checkInverse <- function(x = matrix(), inv = matrix()){
+    #first check if it's a square matrix
+    if(nrow(x) != ncol(x)) return (FALSE)
+    
+    #if they are equal, then matrix multiplication should give the identity matrix
+    prod <- matrixMultiply(x, inv)
+    
+    #verify that prod is the identity matrix
+    for(j in 1:ncol(prod)){
+        for (i in 1:nrow(prod)){
+            if(i == j){
+                #should be one
+                if (prod[i,j] != 1) return (FALSE)
+            }
+            else{
+                #should be zero
+                if (prod[i,j] != 0) return (FALSE)
+            }
+        }
+    }
+    
+    #got to the end with no problems, return true
+    return (TRUE)
 }
