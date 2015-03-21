0c1b12e4626264daa6e2a03ebbef6ff4c7423ee7
whatisthelemon <whatisthelemon@gmail.com>
2015-03-21 08:13:53 -0400

    Filled in functions in cachematrix. I believe that I finished the main assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -4,9 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+    #determine if x is square
     if(nrow(x) != ncol(x)) stop(error="it is only possible to find the inverse of a square matrix")
     
+    #set initial values to null
+    inv <- NULL
     
+    #function to set
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    
+    #function to get (value of x)
+    get <- function() x
+    
+    #function to set inverse
+    setinv <- function(tempinv) inv <<- tempinv
+    
+    #function to get inverse
+    getinv <- function() inv
+    
+    #return a list of functions
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
@@ -14,6 +34,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-    #determine if x is square
-
+    
+    #check if data is in cache
+    inv <- x$getinv()
+    if(!is.null(inv)) {
+        message("getting cached data")
+        return(inv)
+    }
+    
+    #function hasn't returned, so data wasn't in cache, hence we should put it there
+    data <- x$get()
+    inv <- solve(data, ...)
+    x$setinv(inv)
+    
+    #now return the inverse
+    inv
 }
